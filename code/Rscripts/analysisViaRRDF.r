9a0064fd7ad7de907d3823200cee6a45c7a0b663
jameshowison <james@howison.name>
2014-04-25 12:02:56 -0500

    A working workflow from RDF to ggplot2 graph.  On our way, finally!

--- code/Rscripts/analysisViaRRDF.r
+++ code/Rscripts/analysisViaRRDF.r
@@ -1,4 +1,6 @@
 library(rrdf)
+library(ggplot2)
+
 
 setwd("/Users/howison/Documents/UTexas/Projects/SoftwareCitations/softcite/")
 
@@ -8,4 +10,8 @@ summarize.rdf(softciteData)
 prefixes <- paste(readLines("code/Rscripts/sparql_prefixes.sparql", encoding="UTF-8"), collapse=" ")
 query <- paste(readLines("code/Rscripts/all_codes_query.sparql", encoding="UTF-8"), collapse=" ")
 
-code_matrix <- sparql.rdf(softciteData, paste(prefixes, query, collapse=" "),rowvarname="selection")
+code_matrix <- sparql.rdf(softciteData, paste(prefixes, query, collapse=" "))
+
+data <- data.frame(code_matrix)
+
+ggplot(data, aes(x=strata,fill=code)) + geom_bar()
